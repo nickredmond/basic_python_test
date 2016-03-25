6739f0c4561abaf76e835e3f7d5aacf4cea9c39c
Nick Redmond <nicholas.john.redmond@gmail.com>
2016-03-25 16:46:11 -0600

    Added call to quit driver

--- test_ui_firefox.py
+++ test_ui_firefox.py
@@ -8,3 +8,4 @@ driver.get("https://www.dealertrack.com")
 driver.find_element(By.XPATH, ".//*[@id='bs-example-navbar-collapse-1']//*[contains(@href, '/contact.html')]").click()
 
 driver.find_element(By.XPATH, ".//*[text()='Schedule a Product Demo']").is_displayed()
+driver.quit()
