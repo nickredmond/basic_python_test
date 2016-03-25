13a2f4edc15b105157d81b0597629817ac3aa803
Nick Redmond <nicholas.john.redmond@gmail.com>
2016-03-25 16:40:45 -0600

    Wrote a basic UI test script

---
+++ test_ui_firefox.py
@@ -0,0 +1,10 @@
+__author__ = 'nicholas.redmond'
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("https://www.dealertrack.com")
+driver.find_element(By.XPATH, ".//*[@id='bs-example-navbar-collapse-1']//*[contains(@href, '/contact.html')]").click()
+
+driver.find_element(By.XPATH, ".//*[text()='Schedule a Product Demo']").is_displayed()
