bea2fce0b3d52b797988e5a117e9ae38a52d7fcd
Nick Redmond <nicholas.john.redmond@gmail.com>
2016-03-28 12:56:02 -0600

    Publishing test results to HTML file, using HTMLTestRunner

--- test_ui_firefox.py
+++ test_ui_firefox.py
@@ -2,25 +2,36 @@ __author__ = 'nicholas.redmond'
 # import os
 # os.system("pip install --user selenium")
 
+import unittest
+import HTMLTestRunner
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import ElementNotVisibleException
 
-driver = webdriver.Firefox()
-driver.implicitly_wait(10)
-driver.get("https://www.dealertrack.com")
-driver.find_element(By.XPATH, ".//*[@id='bs-example-navbar-collapse-1']//*[contains(@href, '/contact.html')]").click()
 
-element = None
-try:
-    # element = WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located(By.XPATH, ".//*[text()='Schedule a Product Demo']")
-    # )
-    element = driver.find_element(By.XPATH, ".//*[text()='Schedule a Product Demo']")
-finally:
-    if not (element and element.is_displayed()):
-        raise ElementNotVisibleException("Product Demo label was not visible.")
+class ContactPageTests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
 
-driver.quit()
+    def test_schedule_demo_label_visible(self):
+        self.driver.get("https://www.dealertrack.com")
+        self.driver.find_element(By.XPATH, ".//*[@id='bs-example-navbar-collapse-1']//*[contains(@href, '/contact.html')]").click()
+
+        element = None
+        try:
+            element = self.driver.find_element(By.XPATH, ".//*[text()='Schedule a Product Demo']")
+        finally:
+            if not (element and element.is_displayed()):
+                raise ElementNotVisibleException("Product Demo label was not visible.")
+
+    def tearDown(self):
+        self.driver.close()
+
+output_file = open('reports/html/index.html', 'wb')
+runner = HTMLTestRunner.HTMLTestRunner(stream=output_file, title="Nick Redmond's First HTML Output",
+                                       description="This is a test of HTML output in Python automated tests.")
+
+suite = unittest.TestSuite()
+suite.addTest(ContactPageTests("test_schedule_demo_label_visible"))
+runner.run(suite)
